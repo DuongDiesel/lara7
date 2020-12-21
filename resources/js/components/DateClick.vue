d2923fd42f6547e6f2fb4cc4658e8711be9f6cae
duongDiesel <duongbgvip2@gmail.com>
2020-12-21 10:20:35 +0900

    install full caren

---
+++ resources/js/components/DateClick.vue
@@ -0,0 +1,48 @@
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [ dayGridPlugin, interactionPlugin ],
+        initialView: 'dayGridMonth',
+        selectable:true,
+        dateClick: this.handleDateSelect,
+      }
+    }
+  },
+  methods:{
+    handleDateSelect(selectInfo) {
+      let calendarApi = selectInfo.view.calendar
+      var url="/dashboard2report/";
+      console.log(selectInfo.dateStr);
+      var myDate = String(selectInfo.dateStr);
+      console.log(myDate);
+      myDate = myDate.split("-");
+      var newDate = new Date(  myDate[0], myDate[1] - 1, myDate[2]);
+      
+      console.log(newDate.getTime());
+      var timestamp = String( newDate.getTime());
+      var res = url+timestamp;
+      //var timestapm = 
+      calendarApi.unselect(); // clear date selection
+      //var url="/https://adminlaravelgithub.herokuapp.com/dashboard2report/"+String(selectInfo.dateStr);
+       window.open(res);
+    },
+    gotoreport(selectInfo){
+        url="/https://adminlaravelgithub.herokuapp.com/dashboardreport/"+String(selectInfo.dateStr);
+        window.location.replace(url);
+        //alert('date click! ' + selectInfo.dateStr);
+    }
+  }
+}
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
